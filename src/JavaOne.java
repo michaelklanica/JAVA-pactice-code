ccac691132d580cf5b768ac10c87ed0f6c049fb8
Michael Klanica <michael.david.klanica@gmail.com>
2020-10-07 13:54:27 -0500

    Java basics review

---
+++ src/JavaOne.java
@@ -0,0 +1,22 @@
+public class JavaOne {
+    public static void  main(String[] args){
+// Java Primitive Data types
+        // byte (number, 1 byte)
+        byte myByte = 127;
+        // short (number, 2 bytes)
+        short myShort = 32000;
+        // int (number, 4 bytes)
+        int myInteger = 200000;
+        // long (number, 8 bytes)
+        long myLong = 100000000;
+        // float (decimal number, 4 bytes)
+        float myFloat = 2.34F;
+        // double (decimal number, 8 bytes)
+        double myDouble = 23.23232;
+        // char (a character, 2 bytes)
+        char myChar = 'x';
+        // boolean (true or false, 1 byte)
+        boolean myBoolean = true;
+
+    }
+}
